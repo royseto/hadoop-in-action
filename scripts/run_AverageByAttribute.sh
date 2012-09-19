f60f1910613afd5ea8599a083dbb185f31f7f13c
Roy Seto <roy.seto@gmail.com>
2012-09-18 20:05:49 -0700

    Added reducer to AverageByAttribute streaming program

--- scripts/run_AverageByAttribute.sh
+++ scripts/run_AverageByAttribute.sh
@@ -7,6 +7,8 @@ hadoop jar $HS_JAR \
 -input patent/apat63_99.txt \
 -output output \
 -mapper 'scripts/AverageByAttributeMapper.py' \
+-reducer 'scripts/AverageByAttributeReducer.py' \
 -file 'scripts/AverageByAttributeMapper.py' \
+-file 'scripts/AverageByAttributeReducer.py' \
 -jobconf mapred.reduce.tasks=1
 
